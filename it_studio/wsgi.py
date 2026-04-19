59ff4cf0f8799200082334bf4ccb8e8dfb84842b
Данила <122486015+GrOwLy-313@users.noreply.github.com>
2026-04-19 16:26:05 +0300

    proverka2

--- it_studio/wsgi.py
+++ it_studio/wsgi.py
@@ -1,4 +1,13 @@
 import os
+import django
 from django.core.wsgi import get_wsgi_application
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'it_studio.settings')
+
+django.setup()
+
+# Применяем миграции автоматически при каждом старте
+from django.core.management import call_command
+call_command('migrate', '--run-syncdb')
+
 application = get_wsgi_application()
