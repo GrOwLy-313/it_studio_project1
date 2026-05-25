459161d7835194463b70b0e861a83c764c25ef50
Данила <122486015+GrOwLy-313@users.noreply.github.com>
2026-05-25 20:05:45 +0300

    fix17

--- core/templatetags/note_extras.py
+++ core/templatetags/note_extras.py
@@ -18,8 +18,10 @@ def get_item_exists(dictionary, key):
         return False
     return key in dictionary or str(key) in dictionary
 
+
 @register.filter
 def dict_get(dictionary, key):
-    """Получить значение из словаря по строковому ключу."""    if not dictionary:
+    """Получить значение из словаря по строковому ключу."""
+    if not dictionary:
         return None
     return dictionary.get(str(key))
